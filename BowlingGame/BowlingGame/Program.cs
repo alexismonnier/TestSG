67ec4e6aeacda00934400ee51c652a5098d82ed0
alexis Monnier <alexis Monnier>
2016-12-13 16:00:28 +0100

    Création des Classes + Tests

---
+++ BowlingGame/BowlingGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int score;
+
+        public void Roll(int nbr)
+        {
+
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class GameTest
+    {
+        private TestContext TestContextInstance;
+        public TestContext TestContext
+        {
+            get { return TestContextInstance; }
+            set { TestContextInstance = value; }
+        }
+
+        [TestMethod]
+        public void TestAll()
+        {
+            Game g = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+            Assert.AreEqual(20, g.Score());
+        }
+
+    }
+}
