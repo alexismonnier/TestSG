40d1fa163f7656eaf19c8feb238b9ead28d4a7c5
alexis Monnier <alexis Monnier>
2016-12-14 11:27:36 +0100

    Add tests + manage Strike/Spare/Last roll

--- BowlingGame/BowlingGame/Program.cs
+++ BowlingGame/BowlingGame/Program.cs
@@ -8,16 +8,48 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int score;
+        private int[] rolls = new int[21];
+        private int current;
 
         public void Roll(int nbr)
         {
-
+            rolls[current++] = nbr;
         }
 
         public int Score()
         {
+            int score = 0;
+            int roll = 0;
+
+            for (int round = 0; round < 10; round++)
+            {
+                if (IsStrike(roll)) //STRIKE
+                {
+                    score += 10 + rolls[roll + 1] + rolls[roll + 2];
+                    roll++;
+                }
+                else if (IsSpare(roll)) //SPARE
+                {
+                    score += 10 + rolls[roll + 2];
+                    roll += 2;
+                }
+                else //NORMAL
+                {
+                    score += rolls[roll] + rolls[roll + 1];
+                    roll += 2;
+                }
+            }
             return score;
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return rolls[roll] == 10;
         }
 
         static void Main(string[] args)
--- BowlingGame/BowlingGameTest/UnitTest1.cs
+++ BowlingGame/BowlingGameTest/UnitTest1.cs
@@ -7,23 +7,114 @@ namespace BowlingGameTest
     [TestClass]
     public class GameTest
     {
-        private TestContext TestContextInstance;
-        public TestContext TestContext
+        //private TestContext TestContextInstance;
+        //public TestContext TestContext
+        //{
+        //    get { return TestContextInstance; }
+        //    set { TestContextInstance = value; }
+        //}
+
+        private Game g;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            get { return TestContextInstance; }
-            set { TestContextInstance = value; }
+            g = new Game();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            g = null;
         }
 
         [TestMethod]
-        public void TestAll()
+        public void TestAll0()
         {
-            Game g = new Game();
+            MultipleRolls(20, 0);
+            Assert.AreEqual(0, g.Score());
+        }
 
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+        [TestMethod]
+        public void TestAll1()
+        {
+            MultipleRolls(20, 1);
             Assert.AreEqual(20, g.Score());
+        }
+
+        [TestMethod]
+        public void TestSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
+            MultipleRolls(17, 0);
+
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [TestMethod]
+        public void TestStrike()
+        {
+            g.Roll(10);
+            g.Roll(3);
+            g.Roll(4);
+            MultipleRolls(16, 0);
+
+            Assert.AreEqual(24, g.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            MultipleRolls(12, 10);
+
+            Assert.AreEqual(300, g.Score());
+        }
+
+        [TestMethod]
+        public void TestNormalGame()
+        {
+            g.Roll(1);
+            g.Roll(2);
+
+            g.Roll(3);
+            g.Roll(4);
+
+            g.Roll(5);
+            g.Roll(5); //spare
+
+            g.Roll(7);
+            g.Roll(2);
+
+            g.Roll(10); //strike
+
+            g.Roll(2);
+            g.Roll(3);
+
+            g.Roll(3);
+            g.Roll(4);
+
+            g.Roll(2);
+            g.Roll(1);
+
+            g.Roll(9);
+            g.Roll(1); //spare
+
+            g.Roll(4);
+            g.Roll(6); //spare
+
+            g.Roll(10); //strike
+
+            Assert.AreEqual(100, g.Score());
+        }
+
+        private void MultipleRolls(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
         }
 
     }
